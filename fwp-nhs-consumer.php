a653ab1a5c8c4b064269bf95a06f38d8d9925541
Charles Johnson <feedback+github@radgeek.com>
2014-06-27 14:42:49 +0000

    Let's get this party started.

---
+++ fwp-nhs-consumer.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: FWP+: NHS Feeds Consumer
+Plugin URI: https://github.com/radgeek/fwp-nhs-consumer
+Description: A FeedWordPress filter that fetches full content and locally caches images from UK NHS feeds you syndicate, and adds additional options to help with workflow in processing content from NHS feeds.
+Author: Charles Johnson
+Version: 2014.0627
+Author URI: http://projects.radgeek.com
+*/
+
